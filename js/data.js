fea9ba708a4a4174d287acbe8bc6c946839bd4d6
gatlahar <78891562+gatlahar@users.noreply.github.com>
2021-02-22 13:12:56 -0500

    Add files via upload

---
+++ js/data.js
@@ -0,0 +1,98 @@
+mainData = [
+    { 
+      id: 1,
+      type: "Photos",
+      src: 'img/gallery/1a.jpeg',
+      tum: 'img/gallery/1a.jpeg',
+      cp: "Skylo Stare"
+    },
+    { 
+      id: 2,
+      type: "Photos",
+      src: '/img/gallery/8a.jpeg',
+      tum: '/img/gallery/8a.jpeg',
+      cp: "Want to Walk"
+    },
+    { 
+      id: 3,
+      type: "Photos",
+      src: 'img/gallery/6a.jpeg',
+      tum: 'img/gallery/6a.jpeg',
+      cp: "Gorgeous Glow"
+    },
+    { 
+      id: 4,
+      type: "Design",
+      src: 'img/gallery/3a.jpeg',
+      tum: 'img/gallery/3a.jpeg',
+      cp: "Dapper Downtown"
+    },
+    { 
+      id: 5,
+      type: "Photos",
+      src: '/img/gallery/11a.jpeg',
+      tum: '/img/gallery/11a.jpeg',
+      cp: "Look Low"
+    },
+    { 
+      id: 6,
+      type: "Photos",
+      src: 'img/gallery/2a.jpeg',
+      tum: 'img/gallery/2a.jpeg',
+      cp: "Stop Staring"
+    },
+    { 
+      id: 7,
+      type: "Design",
+      src: 'img/gallery/4a.jpeg',
+      tum: 'img/gallery/4a.jpeg',
+      cp: "Just Chillin"
+    },
+    { 
+      id: 8,
+      type: "Photos",
+      src: 'img/gallery/7a.jpeg',
+      tum: 'img/gallery/7a.jpeg',
+      cp: "Oop, Watch out"
+    },
+    { 
+      id: 9,
+      type: "Photos",
+      src: '/img/gallery/16a.jpeg',
+      tum: '/img/gallery/16a.jpeg',
+      cp: "Common Now, Smile"
+    },{ 
+      id: 10,
+      type: "Design",
+      src: 'img/gallery/5a.jpeg',
+      tum: 'img/gallery/5a.jpeg',
+      cp: "Caught Your Eye"
+    }//,{ 
+      //id: 11,
+      //type: "Design",
+      //src: 'file:///Users/Harik/Desktop/DSC_5317.jpeg',
+      //tum: 'file:///Users/Harik/Desktop/DSC_5317.jpeg',
+      //cp: "Nice Photo"
+    //},
+    //{ 
+      //id: 12,
+      //type: "Photoshop",
+      //src: 'file:///Users/Harik/Desktop/DSC_6665.jpeg',
+      //tum: 'file:///Users/Harik/Desktop/DSC_6665.jpeg',
+      //cp: "Nice Photo"
+    //},
+    //{ 
+      //id: 13,
+      //type: "Photos",
+      //src: 'file:///Users/Harik/Desktop/DSC_0032.jpeg',
+      //tum: 'file:///Users/Harik/Desktop/DSC_0032.jpeg',
+      //cp: "Nice Photo"
+    //},
+    //{ 
+      //id: 14,
+      //type: "Photos",
+      //src: 'file:///Users/Harik/Desktop/DSC_6659.jpeg',
+      //tum: 'file:///Users/Harik/Desktop/DSC_6659.jpeg',
+      //cp: "Nice Photo"
+    //}
+  ];
